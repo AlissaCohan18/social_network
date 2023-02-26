4fa0b83aba72a0ff86f6b0c5ea01bea3f0b08ffa
Alissa Cohan <alissacohan18@gmail.com>
2023-02-25 19:46:30 -0500

    added remaining TODO's for Thought and User controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,27 +1,37 @@
 const { Thought } = require("../models");
 
-
 const thoughtController = {
-  
-    //get all thoughts (GET /api/thoughts)
-    getAllThoughts(req, res) {
-      Thought.find({})
-        .then((dbData) => res.json(dbData))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-  
-       //create Thought   (POST /api/thoughts)
-    createThought({ body }, res) {
-      Thought.create(body)
-        .then((dbData) => res.json(dbData))
-        .catch((err) => res.status(400).json(err));
-    },
-    
+  //get all thoughts (GET /api/thoughts)
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  //TODO: get a single thought by ID (GET /api/thoughts/:ID)
+
+  //create Thought   (POST /api/thoughts)
+  //TODO:  (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((dbData) => res.json(dbData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //TODO: update a thought by its ID (PUT /api/thoughts/:ID)
+
+  //TODO: Delete a thought by its ID (DELETE /api/thoughts/:ID)
+
+  //TODO: create a reaction stored in a single thought's reactions array field
+    //(POST /api/thoughts/:thoughtId/reactions)
+
+  //TODO: pull and remove a reaction by the reaction's reactionId value
+    //(DELETE /api/thoughts/:thoughtId/reactions)
+
 
   };
-  
-  module.exports = thoughtController;
-  
+
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,27 +1,35 @@
 const { User } = require("../models");
 
-
 const userController = {
-  
-    //get all users (GET /api/users)
-    getAllUsers(req, res) {
-      User.find({})
-        .then((dbData) => res.json(dbData))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-  
-       //create User   (POST /api/users)
-    createUser({ body }, res) {
-      User.create(body)
-        .then((dbData) => res.json(dbData))
-        .catch((err) => res.status(400).json(err));
-    },
-    
+  //get all users (GET /api/users)
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-  };
-  
-  module.exports = userController;
-  
+  //TODO: get a user by ID (GET /api/users/:ID)
+
+
+  //create User   (POST /api/users)
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbData) => res.json(dbData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+//TODO: update a user by ID (PUT /api/users/:ID)
+
+
+//TODO: Delete a user by ID (DELETE /api/users/:ID)
+
+//TODO: add new friend to user's friend list (POST /api/users/:userId/friends/:friendId)
+
+//TODO: delete a friend from a user's friend list (DELETE /api/users/:userId/friends/:friendId)
+
+};
+
+module.exports = userController;
